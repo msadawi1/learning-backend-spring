7dabcec87b529311ad8ca71aa6abb49d4bab5da3
MSadawi <sadawe147@gmail.com>
2026-04-02 18:30:52 +0800

    HTTP Request Path Variables

--- section3/backend/src/main/java/com/example/backend/controller/Demo.java
+++ section3/backend/src/main/java/com/example/backend/controller/Demo.java
@@ -2,15 +2,25 @@ package com.example.backend.controller;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
+
 @RestController
 public class Demo {
-    @GetMapping("/hello")
-    public String get(@RequestHeader("User-Agent") String userAgent) {
-        return "GET: API Working. User-Agent: " + userAgent;
+    // Support multiple paths and custom names and optional variables
+    @GetMapping({"/users/{userId}", "/users/{userId}/posts/{postId}"})
+    public String get(@PathVariable(name = "userId") Long customerId, @PathVariable(required = false) String postId) {
+        String response = "";
+        if (postId == null) {
+            response = "User ID is " + customerId;
+        } else  {
+            response = "User ID is " + customerId + " and Post ID is " + postId;
+        }
+        return response;
     }
 
-    @PostMapping("/hello")
-    public String post(@RequestHeader("User-Agent") String userAgent, @RequestBody String body) {
-        return "POST: API Working. User-Agent: " + userAgent + ", body: " + body;
+    // Use a path variable map as shortcut
+    @GetMapping("/users/{userId}/address/{addressId}")
+    public String get(@PathVariable Map<String,String> pathVariables) {
+        return "User ID is " + pathVariables.get("userId") + " and Address ID is " + pathVariables.get("addressId");
     }
 }
