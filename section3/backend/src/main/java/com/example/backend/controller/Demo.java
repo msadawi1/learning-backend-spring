d5fcf953435d7c2cab8157778a833f64b3664f66
MSadawi <sadawe147@gmail.com>
2026-04-02 11:52:52 +0800

    Spring Boot Auto-Configuration & application.properties

--- section3/backend/src/main/java/com/example/backend/controller/Demo.java
+++ section3/backend/src/main/java/com/example/backend/controller/Demo.java
@@ -3,10 +3,15 @@ package com.example.backend.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.Callable;
+
 @RestController
 public class Demo {
     @GetMapping("/hello")
-    public String index() {
-        return "<h1>API Working</h1>";
+    public Callable<String> index() {
+        return () -> {
+            Thread.sleep(3000);
+            return "<h1>API Working</h1>";
+        };
     }
 }
